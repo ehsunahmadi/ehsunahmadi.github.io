7242fd76772e02ae2d85c0ee0332da266888ada5
ehsun <ehsun.ahmadi@gmail.com>
2025-03-02 18:09:57 +0330

    fix config file dup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,11 @@
+// @ts-check
+import mdx from '@astrojs/mdx';
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
+import react from '@astrojs/react';
 export default defineConfig({
 	site: 'https://ehsunahmadi.github.io',
+	integrations: [mdx(), sitemap(), react()],
 });
